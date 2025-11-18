88cfdc36346dd3748ba64f92a6daa3226a6d2fde
dudu-theman <dustanley07@gmail.com>
2025-11-18 16:15:23 -0600

    add ai generated titles

--- backend/helper_functions/song_maker.py
+++ backend/helper_functions/song_maker.py
@@ -2,8 +2,12 @@
 from dotenv import load_dotenv
 import os
 import requests
+from anthropic import Anthropic
 
 load_dotenv()
+
+client = Anthropic()
+
 api_key = os.getenv("SUNO_API_KEY")
 url = "https://api.sunoapi.org/api/v1/generate"
 # payload = {
@@ -26,9 +30,10 @@ headers = {
 #            "headers": headers}
 
 def make_song(query):
+    title = make_title_name(query)
     payload = {
         "prompt": f"{query}",
-        "title": "Lofi beat", 
+        "title": f"{title}", 
         "customMode": True,
         "instrumental": True,
         "model": "V5",
@@ -36,4 +41,20 @@ def make_song(query):
     }
     response = requests.post(url, json=payload, headers=headers)
     return response
-            
+
+def make_title_name(query):
+    prompt = (f"I am generating a song with the following prompt: <prompt>{query}</prompt>. " 
+            "Come up with a short title name for the song. Only give me the title of the song, "
+            "and nothing else. For example:\n"
+            "prompt: 'Upbeat instrumental beat with great vibes' response: 'Energy Overload'")
+    response = client.messages.create(
+        model="claude-3-haiku-20240307",
+	    max_tokens=50,
+	    messages = [
+	        {"role": "user", "content":  f"{prompt}"}
+        ],
+        temperature = 1
+    )
+    return response.content[0].text 
+    
+print(make_title_name("Calm lofi beat with rain and wind sounds in the background."))
